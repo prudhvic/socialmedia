c546f7bc58a6b4fee086e95304343ccebeae153a
prudhvi <prudhvichinnam101@gmail.com>
2021-07-22 13:20:10 +0530

    first-commit

--- src/components/Sports.js
+++ src/components/Sports.js
@@ -3,11 +3,11 @@ import Articles from "./Articles";
 import { useState, useEffect } from "react";
 const Sports = () => {
   let [News, setNews] = useState([]);
-  let [query, setQuery] = useState("sports");
+  
   let [isLoading,setIsLoading]=useState(true)
   useEffect(() => {
      
-      let url=`https://newsapi.org/v2/everything?q=${query}&from=2021-07-20&to=2021-07-22&sortBy=popularity&apiKey=48ea256b9eca48698443fd521a86edc0`
+      let url=`https://newsapi.org/v2/everything?q=tesla&from=2021-07-20&to=2021-07-22&sortBy=popularity&apiKey=48ea256b9eca48698443fd521a86edc0`
     fetch(url)
       .then((response) => {
         return response.json();
